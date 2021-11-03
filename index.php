0ac3107d30d9ec30226615a770a69d5f5fee0aa8
kadowning1 <kadowning1@gmail.com>
2021-11-03 14:02:57 -0400

    top commits this week no doubt

--- index.php
+++ index.php
@@ -13,36 +13,11 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$route = $_GET["route"];
-
-if ($route != "") {
-    switch ($route) {
-        case "create":
-            createHero($_GET["name"], $_GET["about_me"], $_GET["biography"], $conn);
-            break;
-            case "all":
-            getAll($_GET["id"], $conn);
-            break;
-        case "read":
-            // readAllHeroes($_GET["id"], $conn);
-            break;
-        case "update":
-            updateHero($_GET["id"], $_GET["name"], $_GET["about_me"], $_GET["biography"], $conn);
-            break;
-        case "delete":
-            deleteHero($_GET["id"], $conn);
-            break;
-        default:
-    }
-
-    readAllHeroes($conn);
-}
 
 // create
 function createHero($name, $about_me, $biography, $conn)
 {
-
-    $sql = "INSERT INTO heroes (name, about_me, biography) VALUES ($name, $about_me, $biography)";
+    $sql = "INSERT INTO heroes (name, about_me, biography) VALUES ('$name', '$about_me', '$biography')";
 
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
@@ -51,13 +26,18 @@ function createHero($name, $about_me, $biography, $conn)
     }
 }
 
+
 // read
 function readAllHeroes($conn)
 {
     $sql = "SELECT * FROM heroes";
     $result = $conn->query($sql);
 
-
+    if ($conn->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
 }
 
 //update
@@ -70,13 +50,13 @@ function updateHero($id, $name, $about_me, $biography, $conn)
     } else {
         echo "Error updating record: " . $conn->error;
     }
-    $conn->close();
 }
 
+//delete
 function deleteHero($id, $conn)
 {
     // sql to delete a record
-    $sql = "DELETE FROM heroes WHERE id=$id";
+    $sql = "DELETE FROM heroes WHERE id='$id'";
 
     if ($conn->query($sql) === TRUE) {
         echo "Record deleted successfully";
@@ -85,15 +65,41 @@ function deleteHero($id, $conn)
     }
 }
 
-function getAll ($conn){
+function getAll($conn)
+{
 
-    $sql = "SELECT * FROM heroes";
+    $sql = "SELECT * FROM heroes"; //inner join
 
- if ($conn->query($sql) === TRUE) {
+    if ($conn->query($sql) === TRUE) {
         echo "Record deleted successfully";
     } else {
         echo "Error deleting record: " . $conn->error;
     }
 }
 
-?>
+$route = $_GET["route"];
+
+if ($route != "") {
+    switch ($route) {
+        case "create":
+            createHero($_POST["name"], $_POST["about_me"], $_POST["biography"], $conn);
+            break;
+        case "all":
+            getAll($_GET["id"], $conn);
+            break;
+        case "read":
+            // readAllHeroes($_GET["id"], $conn);
+            break;
+        case "update":
+            updateHero($_GET["id"], $_GET["name"], $_GET["about_me"], $_GET["biography"], $conn);
+            break;
+        case "delete":
+            deleteHero($_GET["id"], $conn);
+            break;
+        default:
+            echo 'Error 404';
+    }
+    // readAllHeroes($conn);
+}
+
+$conn->close();
