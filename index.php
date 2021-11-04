ef2066018bfebd6ff257a5a35d25bd665059f32e
kadowning1 <kadowning1@gmail.com>
2021-11-04 12:04:01 -0400

    add functions

--- index.php
+++ index.php
@@ -11,10 +11,10 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-function createHero($name, $about_me, $biography, $conn)
+function createHero($name, $about_me, $biography)
 {
     $sql = "INSERT INTO heroes (name, about_me, biography) VALUES ('$name', '$about_me', '$biography')";
-
+    global $conn;
     if ($conn->query($sql) === TRUE) {
         echo "New record created successfully";
     } else {
@@ -22,11 +22,23 @@ function createHero($name, $about_me, $biography, $conn)
     }
 }
 
-function readAllHeroes($conn)
+function addAbility($ability)
+{
+    $sql = "INSERT INTO ability_type (ability) VALUES ('$ability')";
+    global $conn;
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+function readAllHeroes()
 {
     $sql = "SELECT * FROM heroes";
+    global $conn;
     $result = $conn->query($sql);
-
+    global $conn;
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             echo "<br> id: " . $row["id"] . " - Hero: " . $row["name"] . " - About Me: " . $row["about_me"] . " - Biography: " . $row["biography"] . "<br>";
@@ -36,10 +48,25 @@ function readAllHeroes($conn)
     }
 }
 
-function updateHero($id, $name, $about_me, $biography, $conn)
+function getAbility()
+{
+    $sql = "SELECT ability FROM ability_type";
+    global $conn;
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            print_r($row);
+        }
+    } else {
+        echo $conn->errors;
+    }
+}
+
+function updateHero($id, $name, $about_me, $biography)
 {
     $sql = "UPDATE heroes SET name='$name', about_me='$about_me', biography='$biography' WHERE id='$id'";
-
+    global $conn;
     if ($conn->query($sql) === TRUE) {
         echo "Record updated successfully";
     } else {
@@ -47,10 +74,32 @@ function updateHero($id, $name, $about_me, $biography, $conn)
     }
 }
 
-function deleteHero($id, $conn)
+function updateAbility($ability, $id)
+{
+    $sql = "UPDATE ability_type SET ability='$ability' WHERE id='$id'";
+    global $conn;
+    if ($conn->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
+}
+
+function deleteHero($id)
 {
     $sql = "DELETE FROM heroes WHERE id='$id'";
+    global $conn;
+    if ($conn->query($sql) === TRUE) {
+        echo "Record deleted successfully";
+    } else {
+        echo "Error deleting record: " . $conn->error;
+    }
+}
 
+function deleteAbility($id)
+{
+    $sql = "DELETE FROM ability_type WHERE id='$id'";
+    global $conn;
     if ($conn->query($sql) === TRUE) {
         echo "Record deleted successfully";
     } else {
@@ -69,6 +118,7 @@ function getAll()
     INNER JOIN ability_type ON abilities.ability_id = ability_type.id)
     GROUP BY heroes.name, heroes.about_me";
     global $conn;
+
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -86,6 +136,18 @@ if ($route != "") {
     switch ($route) {
         case "create":
             createHero($_POST["name"], $_POST["about_me"], $_POST["biography"], $conn);
+            break;
+        case "getability":
+            getAbility($conn);
+            break;
+        case "addability":
+            addAbility($_POST["ability"], $conn);
+            break;
+        case "updateAbility":
+            updateAbility($_POST["ability"], $_POST['id'], $conn);
+            break;
+        case "deleteAbility":
+            deleteAbility($_GET['id'], $conn);
             break;
         case "all":
             getAll($_GET["id"], $conn);
